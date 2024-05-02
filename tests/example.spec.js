01efd9e6eefb72dc276c762f2ea30e1ee686c3f4
lrbg <lrbg_02@hotmail.com>
2024-05-01 18:59:45 -0600

    añadi cambios 1

---
+++ tests/example.spec.js
@@ -0,0 +1,27 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+test('test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Writing tests', exact: true }).click();
+  await page.getByRole('link', { name: 'Emulation' }).click();
+  console.log("algo");
+});
