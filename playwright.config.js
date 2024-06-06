a54cb18393a70bb1776d1eb55714182fb5af3a0d
lrbg <lrbg_02@hotmail.com>
2024-06-05 23:55:20 -0600

    Resolve merge conflict in playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,9 +1,5 @@
 // @ts-check
-<<<<<<< HEAD
-const { defineConfig, devices } = require('@playwright/test');
-=======
 const { defineConfig, devices } = require("@playwright/test");
->>>>>>> develop
 
 /**
  * Read environment variables from file.
@@ -15,11 +11,7 @@ const { defineConfig, devices } = require("@playwright/test");
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-<<<<<<< HEAD
-  testDir: './tests',
-=======
   testDir: "./tests",
->>>>>>> develop
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -29,43 +21,19 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-<<<<<<< HEAD
-  reporter: 'html',
-=======
   reporter: "html",
->>>>>>> develop
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-<<<<<<< HEAD
-    trace: 'on-first-retry',
-=======
     trace: "on-first-retry",
-    hadless: process.env.CI ? true : false,
->>>>>>> develop
+    headless: process.env.CI ? true : false,
   },
 
   /* Configure projects for major browsers */
   projects: [
-<<<<<<< HEAD
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-=======
     //{
     // name: 'chromium',
     // use: { ...devices['Desktop Chrome'] },
@@ -80,7 +48,6 @@ module.exports = defineConfig({
     // name: 'webkit',
     // use: { ...devices['Desktop Safari'] },
     //},
->>>>>>> develop
 
     /* Test against mobile viewports. */
     // {
@@ -93,21 +60,10 @@ module.exports = defineConfig({
     // },
 
     /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-<<<<<<< HEAD
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-=======
     {
       name: "Google Chrome",
       use: { ...devices["Desktop Chrome"], channel: "chrome" },
     },
->>>>>>> develop
   ],
 
   /* Run your local dev server before starting the tests */
@@ -117,7 +73,3 @@ module.exports = defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> develop
